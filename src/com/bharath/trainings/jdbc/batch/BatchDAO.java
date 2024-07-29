6d17a05e83634c4da38e9b44455fba15dc5b4569
felix2354 <felix_meza@hotmail.com>
2024-07-29 01:23:50 -0600

    iline of comments was deleted

--- src/com/bharath/trainings/jdbc/batch/BatchDAO.java
+++ src/com/bharath/trainings/jdbc/batch/BatchDAO.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-//import java.util.Iterator;
 
 public class BatchDAO {
 	public static void main(String[] args)   {
